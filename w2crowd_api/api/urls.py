ab1da2aa56fdd203112925e873d77cfeff90e280
H4ckerman <paletovicion@hotmail.com>
2023-10-11 11:20:39 -0600

    add models, serializers and migrations - refactor querys using bulk_create

--- w2crowd_api/api/urls.py
+++ w2crowd_api/api/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
-from api.views import ProductViewSet
+from api.views import (
+    ProductViewSet,
+    UploadFileView,
+    WarehouseViewSet,
+    WarehouseProductViewSet,
+    OrderSellViewSet,
+    OrderSellProductViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register(r"products", ProductViewSet)
+router.register(r"warehouses", WarehouseViewSet)
+router.register(r"warehouses_products", WarehouseProductViewSet)
+router.register(r"orders_sell", OrderSellViewSet)
+router.register(r"orders_sell_products", OrderSellProductViewSet)
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("upload_file/", UploadFileView.as_view(), name="upload_file"),
 ]
--- w2crowd_api/api/views.py
+++ w2crowd_api/api/views.py
@@ -1,7 +1,106 @@
+from traceback import print_exc
+from django.db import transaction
 from rest_framework import viewsets
-from .serializers import ProductSerializer
-from .models import Product
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FileUploadParser
+from .serializers import (
+    OrderSellProductSerializer,
+    ProductSerializer,
+    WarehouseSerializer,
+    OrderSellSerializer,
+    WarehouseProductSerializer,
+)
+from api.models import OrderSellProduct, Product, Warehouse, OrderSell, WarehouseProduct
+from rest_framework import status
+import pandas as pd
+
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().order_by('id')
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class WarehouseViewSet(viewsets.ModelViewSet):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+
+
+class WarehouseProductViewSet(viewsets.ModelViewSet):
+    queryset = WarehouseProduct.objects.all()
+    serializer_class = WarehouseProductSerializer
+
+
+class OrderSellViewSet(viewsets.ModelViewSet):
+    queryset = OrderSell.objects.all()
+    serializer_class = OrderSellSerializer
+
+
+class OrderSellProductViewSet(viewsets.ModelViewSet):
+    queryset = OrderSellProduct.objects.all()
+    serializer_class = OrderSellProductSerializer
+
+
+class UploadFileView(APIView):
+    parser_classes = (MultiPartParser, FileUploadParser)
+
+    def post(self, request):
+        try:
+            uploaded_file = request.data["file"]
+            df = pd.read_excel(uploaded_file)
+            df_products = df.iloc[:, 2:-1]
+            skus = df_products.columns
+            rows = df.values.tolist()[:-1]
+            products = df_products.values.tolist()
+
+            with transaction.atomic():
+                order_sells = []
+                order_sell_products = []
+
+                for idr, row in enumerate(rows):
+                    warehouse, _ = Warehouse.objects.get_or_create(
+                        sub_stock=row[0],
+                        defaults={
+                            "name": row[1],
+                        },
+                    )
+
+                    order_sell = OrderSell(warehouse=warehouse, status="listo")
+                    order_sells.append(order_sell)
+                    for idx, quantity_product in enumerate(products[idr]):
+                        if (
+                            not isinstance(quantity_product, float)
+                            or not quantity_product > 1
+                        ):
+                            continue
+                        sku = skus[idx]
+
+                        product, _ = Product.objects.get_or_create(
+                            sku=sku,
+                            defaults={"name": "", "imei": ""},
+                        )
+
+                        order_sell_product = OrderSellProduct(
+                            order_sell=order_sell,
+                            product=product,
+                            quantity=quantity_product,
+                        )
+                        order_sell_products.append(order_sell_product)
+
+                OrderSell.objects.bulk_create(order_sells)
+                OrderSellProduct.objects.bulk_create(order_sell_products)
+
+            return Response(
+                {"status": "File uploaded successfully"}, status=status.HTTP_200_OK
+            )
+        except pd.errors.ParserError as e:
+            return Response(
+                {"error": "Invalid file format: " + str(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        except Exception as e:
+            print_exc()
+            return Response(
+                {"error": "An error occurred: " + str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
