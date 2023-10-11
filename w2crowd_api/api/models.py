d2205543973d52f8e4fdc15572608e397e4f220d
H4ckerman <paletovicion@hotmail.com>
2023-10-11 11:15:34 -0600

    add models, serializers and migrations

--- w2crowd_api/api/models.py
+++ w2crowd_api/api/models.py
@@ -2,7 +2,37 @@ from django.db import models
 
 
 class Product(models.Model):
+    sku = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
-    stock = models.IntegerField()
-    sku = models.CharField(max_length=255)
     imei = models.CharField(max_length=255)
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=255)
+    sub_stock = models.CharField(max_length=255, primary_key=True)
+    products = models.ManyToManyField(Product, through="WarehouseProduct")
+
+
+class WarehouseProduct(models.Model):
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class OrderSell(models.Model):
+    products = models.ManyToManyField(Product, through="OrderSellProduct")
+    status = models.CharField(max_length=100,default="pending")
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderSellProduct(models.Model):
+    order_sell = models.ForeignKey(OrderSell, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
--- w2crowd_api/api/serializers.py
+++ w2crowd_api/api/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from .models import Product
+from .models import OrderSellProduct, Product, Warehouse, OrderSell, WarehouseProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
+
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = "__all__"
+
+
+class WarehouseProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WarehouseProduct
+        fields = "__all__"
+
+
+class OrderSellSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderSell
+        fields = "__all__"
+
+
+class OrderSellProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderSellProduct
         fields = "__all__"
